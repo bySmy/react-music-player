e8f4184509a840e2ef4aa1d2850e1119dce5886a
Sumeyye Rajabli <sumeyye.recebli@gmail.com>
2021-07-11 01:29:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useRef } from 'react';
+import './styles/app.scss';
+import Player from './components/Player';
+import Song from './components/Song';
+import data from "./data";
+import Library from './components/Library';
+import Nav from './components/Nav';
+
+
+function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+  const [songInfo, setSongInfo]= useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus, setLibraryStatus]= useState(false);
+  
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime; 
+    const duration = e.target.duration;
+    // Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round((roundedCurrent / roundedDuration)*100)
+  
+    setSongInfo({...songInfo,
+                currentTime: current, 
+                duration: duration,
+                animationPercentage: animationPercentage
+              });
+  };
+
+  const songEndHandler = async () =>{
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id); 
+      await  setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+      if(isPlaying) audioRef.current.play();
+
+  };
+
+  return (
+    <div className={`App ${libraryStatus ? "library-active" : "" }`}>
+      <Nav libraryStatus={libraryStatus}
+           setLibraryStatus={setLibraryStatus}/>
+      <Song currentSong={currentSong} />
+      <Player isPlaying={isPlaying} 
+             setIsPlaying={setIsPlaying} 
+             currentSong={currentSong} 
+             audioRef = {audioRef}
+             songInfo={songInfo}
+             setSongInfo={setSongInfo}
+             songs ={songs}
+             setCurrentSong={setCurrentSong}
+             setSongs= {setSongs}
+             />
+      <Library  songs ={songs}
+               setCurrentSong={setCurrentSong}
+               audioRef={audioRef} 
+               isPlaying={isPlaying}
+               setSongs= {setSongs}
+               libraryStatus={libraryStatus}
+                /> 
+      <audio onTimeUpdate={timeUpdateHandler}
+                 onLoadedMetadata={timeUpdateHandler} 
+                 ref= {audioRef} 
+                 src={currentSong.audio}
+                 onEnded={songEndHandler}>
+                   </audio>    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,107 @@
+import { v4 as uuidv4 } from "uuid" ;
+
+function chillHop() {
+    return [
+      {
+        name:"Goodmorning",
+        cover:"https://chillhop.com/wp-content/uploads/2020/07/aa103b43660077c66e65a1edc009844171bbd9c6-300x300.jpg",
+        artist:"Aviino",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=9488",
+        color:["#CE90B5","#4987BB"],
+        id: uuidv4(),
+        active: false
+      },
+
+      {
+        name:"Opia",
+        cover:"https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+        artist:"Toonorth",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=17946",
+        color: ["#3B4E6F","#f7e652" ],
+        id: uuidv4(),
+        active: true,
+      },
+
+    
+         
+      {
+        name:"Got It",
+        cover:"https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+        artist:"Misha,Jussi Halme",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=15041",
+        color: ["#f3962c","#3A3C54" ],
+        id: uuidv4(),
+        active:  false
+      }, 
+      {
+        name: "Maple",
+        cover:"https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+        artist:"Arbour,Aarigod",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=13015",
+        color: ["#A9C274","#43432B"],
+        id: uuidv4(),
+        active: false 
+      },
+      
+      {
+        name:"Daylight",
+        cover:"https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-300x300.jpg",
+        artist:"Aiguille",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=9272",
+        color: ["#D97591","#695B98" ],
+        id: uuidv4(),
+        active:  false
+      },
+
+        {
+            name:"Goodmorning",
+            cover:"https://chillhop.com/wp-content/uploads/2020/07/aa103b43660077c66e65a1edc009844171bbd9c6-300x300.jpg",
+            artist:"Aviino",
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=9488",
+            color:["#CE90B5","#4987BB"],
+            id: uuidv4(),
+            active: false
+          },
+      
+      {
+        name:"To The Moon & Back",
+        cover:"https://chillhop.com/wp-content/uploads/2020/07/5c2d5b05dfc98afb5ed850ca918f732445b8ca1e-1024x1024.jpg",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=8717",
+        color: ["#D97591","#695B98" ],
+        id: uuidv4(),
+        active:  false
+      },
+      {
+        name:"Travelbag",
+        cover:"https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+        artist:"Mommy,Sleep Fish",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=19058",
+        color: ["#618FD2","#f5d0d0" ],
+        id: uuidv4(),
+        active:  false
+      },
+
+        {
+            name:"Lavish",
+            cover:"https://chillhop.com/wp-content/uploads/2021/05/94a210b97ac05e5f05e1ce784a1a9ecf90168d03-1024x1024.jpg",
+            artist:"Toonorth",
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=17933",
+            color:["#1F2025","#B4CFC8"],
+            id: uuidv4(),
+            active: false
+          },
+      
+      {
+        name:"Festa",
+        cover:"https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+        artist:"Misha,Jussi Halme,Soul food Horns",
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=15041",
+        color: ["#f3962c","#3A3C54" ],
+        id: uuidv4(),
+        active:  false
+      },
+
+
+    ];
+}
+export default chillHop;
